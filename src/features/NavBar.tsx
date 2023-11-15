3fd8a7ef06f31d153be3902827a56dd161f7d091
Alexander Sibhatu <sibhatualexander@gmail.com>
2023-11-15 22:30:52 +0000

    navigation bar done

--- src/features/NavBar.tsx
+++ src/features/NavBar.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
+import { useTheme  } from '../context/ThemeContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import '../App.css'
 
 type Props = {}
 
 const NavBar = (props: Props) => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <nav id="nav">
-        <ul>
-          <li>Home</li>
-          <li>Favourites</li>
-        </ul>
-      </nav>
+    <nav className={ theme === 'light'? 'dark-navbar' : "light-navbar" }>
+      <ul>
+        <li>
+          Home
+        </li>
+        <li>
+          Favourites
+        </li>
+        <li>
+        <FontAwesomeIcon 
+            className='themeIcon'
+            icon={theme === 'light'? faMoon : faSun} 
+            onClick={toggleTheme}
+          />
+        </li>
+      </ul>    
+    </nav>
   )
 }
 
