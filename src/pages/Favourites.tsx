9971787ce93c88d690d862b6f4e42dd38256b95a
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-10-25 10:45:21 +0100

    country component used

--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -1,28 +1,17 @@
 import { useSelector } from 'react-redux'
 import { CountryT, CountriesT } from '../type/type'
-import { removeFavourite, selectFavouriteCountries }  from '../redux/counter/counterSlice'
+import { selectFavouriteCountries }  from '../redux/counter/counterSlice'
 import { ThemeProvider } from './../context/ThemeContext';
 import Footer from '../features/Footer'
 import NavBar from '../features/NavBar'
-import { useDispatch } from 'react-redux';
+import Country from '../components/Country';
 
 
 
 const Favourites = () => {
-  // const favouriteCountries: CountriesT = useSelector((state: RootState) => 
-  //     state.favourites?.favouriteCountries || [])
   const favouriteCountries: CountriesT = useSelector(selectFavouriteCountries) || [];
   console.log(favouriteCountries)
 
-  const dispatch = useDispatch()
-
-  // useEffect(() => {
-  //   console.log('Updated Favourite Countries:', favouriteCountries);
-  // }, [favouriteCountries]);
-
-  const handleRemoveClick = (country: CountryT) => {
-    dispatch(removeFavourite(country));
-  };
 
   return (
     <ThemeProvider >
@@ -32,15 +21,11 @@ const Favourites = () => {
           {favouriteCountries.length === 0 ?(
             <p>No favourite countries added yet.</p>
           ) : (
-            <p>
+            <div className='fav-countries'>
               {favouriteCountries?.map((country: CountryT) => (
-                <li key={country.name.common}>
-                  {country.name.common}
-                  <button onClick={() => handleRemoveClick(country)}>Remove</button>
-                </li>
-                
+                <Country key={country.name.common} country={country} />
               ))}
-            </p>
+            </div>
           )}
         <Footer />
       </div>
